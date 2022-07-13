5e638d04bc811b445db31297de05221e9be445f6
manojmanoj421 <109210900+manojmanoj421@users.noreply.github.com>
2022-07-13 15:24:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //════════════(00)════════════════\\
 //
-//          RED DRAGON v6.0
+//                ᴹᴿ᭄₥₳₦ØJ৫᭄̊̊࿓࿔
 //
 //════════════(00)════════════════\\
 
@@ -19,31 +19,31 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `My name is ᴹᴿ᭄₥₳₦ØJ৫᭄̊̊࿓࿔ BOT
+My Creator is Manoj Madhusankha` //Costomize Alive Message (type your message in `` )
 
 global.owner = ['+380944814219'] //Owner number in aive msg
 global.premium = ['+380944814219'] //Owner Number info
 global.ownernomer = '+380944814219' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.ownername = 'MANOJ MADHUSANKHA' //Owner Name
+global.botname = ' ᴹᴿ᭄₥₳₦ØJ৫᭄̊̊࿓࿔' //Bot Name
 
-global.button = '🤭Youtube📍' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://youtu.be/E5C-lrl_g9I' //Change Your Button's URL
+global.button = 'Whatsapp' //Costomize A Button Name In Alive Message
+global.btnurl = 'https://wa.me/94775798443' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = 'ᴹᴿ᭄₥₳₦ØJ৫᭄̊̊࿓࿔' //Bot Pacage Name
+global.author = 'MANOJ' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '©_Kumuthu.' //In list menu
+global.footer = 'ᴹᴿ᭄₥₳₦ØJ৫᭄̊̊࿓࿔' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
+global.ig = 'https://github.com' //Github Account!
 
 global.mess = { // COSTOMIZE WITH YOUR LANGUAGE
 	
